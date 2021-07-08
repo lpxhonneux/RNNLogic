52ac96f6d143b26b0bd84a658b2ad0bb4bf885bc
Louis-Pascal Xhonneux <lpxhonneux@gmail.com>
2021-07-08 11:55:25 -0400

    Add hydra + Rule freq. plotting

--- dataloader.py
+++ dataloader.py
@@ -1,46 +1,267 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+from torch_sparse import spmm
+import rnnlogic_ext as pyrnnlogic
+import numpy as np
+import os
+import gc
+import random
+
+class KnowledgeGraph:
+    def __init__(self, args):
+        self.args = args
+        data_path = args.data_path
+
+        self.entity2id = dict()
+        self.relation2id = dict()
+
+        with open(os.path.join(data_path, 'entities.dict')) as fi:
+            for line in fi:
+                id, entity = line.strip().split('\t')
+                self.entity2id[entity] = int(id)
+
+        with open(os.path.join(data_path, 'relations.dict')) as fi:
+            for line in fi:
+                id, relation = line.strip().split('\t')
+                self.relation2id[relation] = int(id)
+
+        self.entity_size = len(self.entity2id)
+        self.relation_size = len(self.relation2id)
+        
+        self.train_facts = list()
+        self.valid_facts = list()
+        self.test_facts = list()
+        self.hr2o = dict()
+        self.hr2v = dict()
+        self.relation2sparse = [[[], []] for k in range(self.relation_size)]
+        self.relation2hr2i = [dict() for k in range(self.relation_size)]
+
+        with open(os.path.join(data_path, "train.txt")) as fi:
+            for line in fi:
+                h, r, t = line.strip().split('\t')
+                h, r, t = self.entity2id[h], self.relation2id[r], self.entity2id[t]
+                self.train_facts.append((h, r, t))
+
+                hr_index = self.encode_hr(h, r)
+                if hr_index not in self.hr2o:
+                    self.hr2o[hr_index] = list()
+                self.hr2o[hr_index].append(t)
+                if hr_index not in self.hr2v:
+                    self.hr2v[hr_index] = list()
+                self.hr2v[hr_index].append(t)
+
+                self.relation2sparse[r][0].append(t)
+                self.relation2sparse[r][1].append(h)
+
+                i = len(self.relation2hr2i[r])
+                ht_index = self.encode_ht(h, t)
+                self.relation2hr2i[r][ht_index] = i
+
+        with open(os.path.join(data_path, "valid.txt")) as fi:
+            for line in fi:
+                h, r, t = line.strip().split('\t')
+                h, r, t = self.entity2id[h], self.relation2id[r], self.entity2id[t]
+                self.valid_facts.append((h, r, t))
+
+                hr_index = self.encode_hr(h, r)
+                if hr_index not in self.hr2v:
+                    self.hr2v[hr_index] = list()
+                self.hr2v[hr_index].append(t)
+
+        with open(os.path.join(data_path, "test.txt")) as fi:
+            for line in fi:
+                h, r, t = line.strip().split('\t')
+                h, r, t = self.entity2id[h], self.relation2id[r], self.entity2id[t]
+                self.test_facts.append((h, r, t))
+
+                hr_index = self.encode_hr(h, r)
+                if hr_index not in self.hr2v:
+                    self.hr2v[hr_index] = list()
+                self.hr2v[hr_index].append(t)
+
+        for r in range(self.relation_size):
+            index = torch.LongTensor(self.relation2sparse[r]).cuda()
+            value = torch.ones(index.size(1)).cuda()
+            self.relation2sparse[r] = [index, value]
+
+        print("Data loading | DONE!")
+
+    def encode_hr(self, h, r):
+        return r * self.entity_size + h
+
+    def decode_hr(self, index):
+        h, r = index % self.entity_size, index // self.entity_size
+        return h, r
+
+    def encode_ht(self, h, t):
+        return t * self.entity_size + h
+
+    def decode_hr(self, index):
+        h, t = index % self.entity_size, index // self.entity_size
+        return h, t
+
+    def get_updated_adjacency(self, r, edges_to_remove):
+        if edges_to_remove == None:
+            return None
+        index = self.relation2sparse[r][0]
+        value = self.relation2sparse[r][1]
+        mask = (index.unsqueeze(1) == edges_to_remove.unsqueeze(-1))
+        mask = mask.all(dim=0).any(dim=0)
+        mask = ~mask
+        index = index[:, mask]
+        value = value[mask]
+        return [index, value]
+
+    def grounding(self, h, r, rule, updated_adjacency):
+        with torch.no_grad():
+            #x = torch.LongTensor(h).cuda()
+            #x = torch.nn.functional.one_hot(x, self.entity_size).transpose(0, 1)
+            x = torch.nn.functional.one_hot(h, self.entity_size).transpose(0, 1)
+            for r_body in rule:
+                index = self.relation2sparse[r_body][0]
+                value = self.relation2sparse[r_body][1]
+                if r_body == r and updated_adjacency != None:
+                    index = updated_adjacency[0]
+                    value = updated_adjacency[1]
+                x = spmm(index, value, self.entity_size, self.entity_size, x)
+        return x
+
+class TrainDataset(Dataset):
+    def __init__(self, args, kg):
+        self.args = args
+        self.kg = kg
+
+        self.r2queries = [[] for r in range(self.kg.relation_size)]
+        for h, r, t in self.kg.train_facts:
+            self.r2queries[r].append((h, r))
+
+        self.r2queries = [list(set(_)) for _ in self.r2queries]
+
+        self.make_batches()
+
+    def make_batches(self):
+        for r in range(self.kg.relation_size):
+            random.shuffle(self.r2queries[r])
+
+        self.batches = list()
+        for r, queries in enumerate(self.r2queries):
+            for k in range(0, len(queries), self.args.predictor_batch_size):
+                start = k
+                end = min(k + self.args.predictor_batch_size, len(queries))
+                self.batches.append(queries[start:end])
+        random.shuffle(self.batches)
+
+    def __len__(self):
+        return len(self.batches)
+
+    def __getitem__(self, idx):
+        return self.batches[idx]
+
+    def get_batch(self, idx):
+        #idx = random.randint(0, len(self.batches) - 1)
+        data = self.batches[idx]
+
+        all_h = torch.LongTensor([_[0] for _ in data])
+        all_r = torch.LongTensor([_[1] for _ in data])
+        target = torch.zeros(len(data), self.kg.entity_size)
+        edges = list()
+        for k, (h, r) in enumerate(data):
+            hr_index = self.kg.encode_hr(h, r)
+            t_index = torch.LongTensor(self.kg.hr2o[hr_index])
+            target[k][t_index] = 1
+            h_index = torch.full_like(t_index, h)
+            edges_ = torch.stack([t_index, h_index], dim=0)
+            edges_.size()
+            edges.append(edges_)
+        edges = torch.cat(edges, dim=-1)
+
+        return all_h, all_r, target, edges
+
+class TestDataset(Dataset):
+    def __init__(self, args, kg, mode):
+        self.args = args
+        self.kg = kg
+
+        if mode == "valid":
+            facts = self.kg.valid_facts
+        elif mode == "test":
+            facts = self.kg.test_facts
+        else:
+            raise ValueError
+
+        r2instances = [[] for r in range(self.kg.relation_size)]
+        for h, r, t in facts:
+            r2instances[r].append((h, r, t))
+
+        self.batches = list()
+        for r, instances in enumerate(r2instances):
+            for k in range(0, len(instances), args.predictor_batch_size):
+                start = k
+                end = min(k + args.predictor_batch_size, len(instances))
+                self.batches.append(instances[start:end])
+
+    def __len__(self):
+        return len(self.batches)
+
+    def __getitem__(self, idx):
+        return self.batches[idx]
+
+    def get_batch(self, idx):
+        #idx = random.randint(0, len(self.batches) - 1)
+        data = self.batches[idx]
+
+        all_h = torch.LongTensor([_[0] for _ in data])
+        all_r = torch.LongTensor([_[1] for _ in data])
+        all_t = torch.LongTensor([_[2] for _ in data])
+
+        mask = torch.ones(len(data), self.kg.entity_size).bool()
+        for k, (h, r, t) in enumerate(data):
+            hr_index = self.kg.encode_hr(h, r)
+            t_index = torch.LongTensor(self.kg.hr2v[hr_index])
+            mask[k][t_index] = 0
+
+        return all_h, all_r, all_t, mask
 
 class RuleDataset(Dataset):
-	def __init__(self, num_relations, relation2rules=None):
-		self.rules = list()
-		self.num_relations = num_relations
-		self.ending_idx = num_relations
-		self.padding_idx = num_relations + 1
-		if relation2rules != None:
-			for rules in relation2rules:
-				for rule in rules:
-					rule_len = rule[0]
-					formatted_rule = [[rule[1 + k] for k in range(rule_len + 1)] + [self.ending_idx], self.padding_idx, rule[-3] + 1e-5]
-					self.rules.append(formatted_rule)
+    def __init__(self, num_relations, relation2rules=None):
+        self.rules = list()
+        self.num_relations = num_relations
+        self.ending_idx = num_relations
+        self.padding_idx = num_relations + 1
+        if relation2rules != None:
+            for rules in relation2rules:
+                for rule in rules:
+                    rule_len = len(rule) - 1
+                    formatted_rule = [[rule[k] for k in range(rule_len + 1)] + [self.ending_idx], self.padding_idx, float(rule[-1])]
+                    self.rules.append(formatted_rule)
 
-	def __len__(self):
-		return len(self.rules)
+    def __len__(self):
+        return len(self.rules)
 
-	def __getitem__(self, idx):
-		return self.rules[idx]
+    def __getitem__(self, idx):
+        return self.rules[idx]
 
-	@staticmethod
-	def collate_fn(data):
-		inputs = [item[0][0:len(item[0])-1] for item in data]
-		target = [item[0][1:len(item[0])] for item in data]
-		weight = [float(item[-1]) for item in data]
-		max_len = max([len(_) for _ in inputs])
-		padding_index = [int(item[-2]) for item in data]
+    @staticmethod
+    def collate_fn(data):
+        inputs = [item[0][0:len(item[0])-1] for item in data]
+        target = [item[0][1:len(item[0])] for item in data]
+        weight = [float(item[-1]) for item in data]
+        max_len = max([len(_) for _ in inputs])
+        padding_index = [int(item[-2]) for item in data]
 
-		for k in range(len(data)):
-			for i in range(max_len - len(inputs[k])):
-				inputs[k].append(padding_index[k])
-				target[k].append(padding_index[k])
+        for k in range(len(data)):
+            for i in range(max_len - len(inputs[k])):
+                inputs[k].append(padding_index[k])
+                target[k].append(padding_index[k])
 
-		inputs = torch.LongTensor(inputs)
-		target = torch.LongTensor(target)
-		weight = torch.Tensor(weight)
-		mask = (target != torch.LongTensor(padding_index).unsqueeze(1))
+        inputs = torch.LongTensor(inputs)
+        target = torch.LongTensor(target)
+        weight = torch.Tensor(weight)
+        mask = (target != torch.LongTensor(padding_index).unsqueeze(1))
 
-		return inputs, target, mask, weight
+        return inputs, target, mask, weight
 
 def Iterator(dataloader):
-	while True:
-		for data in dataloader:
-			yield data
+    while True:
+        for data in dataloader:
+            yield data
--- run.py
+++ run.py
@@ -1,63 +1,40 @@
-import argparse
-import random
-from model import KnowledgeGraph, RuleMiner, RNNLogic
+import os
 
-def parse_args(args=None):
-	parser = argparse.ArgumentParser(
-		description='RNNLogic',
-		usage='run.py [<args>] [-h | --help]'
-	)
+from omegaconf import DictConfig, OmegaConf
+import hydra
 
-	parser.add_argument('--cuda', action='store_true', help='Use GPU.')
+from model import RNNLogic
+from dataloader import KnowledgeGraph, TrainDataset, TestDataset
+import rnnlogic_ext as pyrnnlogic
 
-	parser.add_argument('--data_path', type=str, default=None, help='Set data path.')
+@hydra.main(config_path="./conf", config_name="config")
+def main(cfg):
+    print("Working directory : {}".format(os.getcwd()))
+    print("Config:")
 
-	parser.add_argument('--max_rule_length', default=3, type=int, help='Set the maximum length of logic rules.')
-	parser.add_argument('--num_generated_rules', default=200, type=int, help='Set the number of logic rules fed to the reasoning predictor at each training iteration.')
-	parser.add_argument('--num_important_rules', default=10, type=int, help='Set the number of high-quality logic rules selected at each iteration.')
-	parser.add_argument('--num_rules_for_test', default=100, type=int, help='Set the number of logic rules for evaluation.')
-	parser.add_argument('--prior_weight', default=1.0, type=float, help='Set the weight of rule prior when inferring the posterior.')
-	parser.add_argument('--num_threads', default=10, type=int, help='Set the number of threads for training.')
-	parser.add_argument('--iterations', default=10, type=int, help='Set the number of training iterations.')
+    print(OmegaConf.to_yaml(cfg))
+    args = cfg
+    args.max_steps_per_iter = 10000
+    args.remove_edges = True
+    args.print_every = 1000
 
-	parser.add_argument('--miner_portion', default=1.0, type=float, help='Set the percentage of training triplets used by rule miners.')
+    p_kg = pyrnnlogic.new_knowledge_graph(args.data_path)
+    p_rm = pyrnnlogic.new_rule_miner(p_kg)
+    pyrnnlogic.run_rule_miner(p_rm, 3, 1.0, 8) # 3 is max rule length, 8 is num of thread.
+    rules = pyrnnlogic.get_logic_rules(p_rm)
 
-	parser.add_argument('--predictor_learning_rate', default=0.01, type=float, help='Set the learning rate for reasoning predictors.')
-	parser.add_argument('--predictor_weight_decay', default=0.0005, type=float, help='Set the weight decay for reasoning predictors.')
-	parser.add_argument('--predictor_temperature', default=100, type=float, help='Set the annealing temperature of reasoning predictors for prediction.')
-	parser.add_argument('--predictor_H_temperature', default=1.0, type=float, help='Set the annealing temperature of reasoning predictors when computing the H score of rules.')
-	parser.add_argument('--predictor_portion', default=1.0, type=float, help='Set the percentage of training triplets used by reasoning predictors.')
+    graph = KnowledgeGraph(args)
+    relation2rules = [[] for r in range(graph.relation_size)]
+    for rule in rules:
+        relation = rule[0]
+        rule = rule + [1.0]
+        relation2rules[relation].append(rule)
 
-	parser.add_argument('--generator_embedding_dim', default=512, type=int, help='Set the embedding dimension for rule generators.')
-	parser.add_argument('--generator_hidden_dim', default=256, type=int, help='Set the hidden dimension for rule generators.')
-	parser.add_argument('--generator_layers', default=1, type=int, help='Set the number of layers for rule generators.')
-	parser.add_argument('--generator_batch_size', default=1024, type=int, help='Set the batch size for rule generators.')
-	parser.add_argument('--generator_epochs', default=10000, type=int, help='Set the number of training epochs for rule generators.')
-	parser.add_argument('--generator_learning_rate', default=0.001, type=float, help='Set learning rate for training rule generators.')
-	parser.add_argument('--generator_tune_epochs', default=100, type=int, help='Set the number of tuning epochs for rule generators.')
-	parser.add_argument('--generator_tune_learning_rate', default=0.00001, type=float, help='Set learning rate for tuning rule generators.')
+    rnnlogic = RNNLogic(args, graph)
+    rnnlogic.m_step(relation2rules)
 
-	return parser.parse_args(args)
+    rnnlogic.train()
+    rnnlogic.evaluate()
 
-def main(args):
-	# Load knowledge graphs.
-	graph = KnowledgeGraph(args.data_path)
-
-	# Mine some relational paths from the knowledge graph.
-	miner = RuleMiner(graph)
-	miner.mine_logic_rules(args.max_rule_length, args.miner_portion, args.num_threads)
-
-	# Pre-train the rule generator on the mined relational paths, 
-	# so that the rule generator will not explore useless rules.
-	rnnlogic = RNNLogic(args, graph)
-	rnnlogic.m_step(miner.get_logic_rules())
-
-	rnnlogic.train()
-	rnnlogic.evaluate()
-	
 if __name__ == '__main__':
-	main(parse_args())
-
-# python run.py --data_path ../data/wn18rr --num_generated_rules 200 --num_rules_for_test 200 --num_important_rules 0 --prior_weight 0.01 --cuda
-# python run.py --data_path ../data/kinship_325 --num_generated_rules 2000 --num_rules_for_test 200 --num_important_rules 0 --prior_weight 0.01 --cuda
-# python run.py --data_path ../data/umls_325 --num_generated_rules 2000 --num_rules_for_test 100 --num_important_rules 0 --prior_weight 0.01 --cuda
+    main()
